5f775ba092a13289fe76fddf246cbe576f14bed8
Dom Williams <dwi3774@googlemail.com>
2015-11-14 13:11:22 +0000

    Initial project creation

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "SSCImageDownloader"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+    
---
+++ src/main/scala/dxw405/ImageDownloader.scala
@@ -0,0 +1,9 @@
+package dxw405
+
+object ImageDownloader
+{
+	def main(args: Array[String])
+	{
+		println(":^)")
+	}
+}
