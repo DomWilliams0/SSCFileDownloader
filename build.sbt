d7e5a84373cabe4149517911b8ffec194e324f32
Dom Williams <dwi3774@googlemail.com>
2015-11-14 14:25:11 +0000

    Added image URL fetching from a site

--- build.sbt
+++ build.sbt
@@ -3,4 +3,5 @@ name := "SSCImageDownloader"
 version := "1.0"
 
 scalaVersion := "2.11.7"
-    
+
+libraryDependencies ++= Seq("org.jsoup" % "jsoup" % "1.7.2")
--- src/main/scala/dxw405/ImageDownloader.scala
+++ src/main/scala/dxw405/ImageDownloader.scala
@@ -1,6 +1,36 @@
 package dxw405
 
+import org.jsoup.Jsoup
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+
+class ImageDownloader
+{
+	def init(): Unit =
+	{
+		val site = "http://www.tutorialspoint.com/scala/"
+
+		val images = getImages(site)
+		println(s"Found ${images.size} images: $images)")
+	}
+
+	/**
+	  * Gets all image URLs on the given page
+	  * @param site The URL of the page
+	  * @return A buffer of all image URLs
+	  */
+	def getImages(site: String): mutable.Buffer[String] =
+	{
+		val doc = Jsoup.connect(site).get()
+		val images = doc.select("img[src~=(?i)\\\\?.(png|jpe?g|gif)]")
+
+		images map (im => im.baseUri() + im.attr("src"))
+	}
+
+}
+
 object ImageDownloader
 {
-	def main(args: Array[String]) = println(":^)")
+	def main(args: Array[String]) = new ImageDownloader().init()
 }
